64104ba682b6eff9ddec28561d400260e618830d
mengzengshan <378077287@qq.com>
2019-04-15 15:24:42 +0800

    RSI 分支

--- functions.py
+++ functions.py
@@ -1,30 +1,59 @@
-import numpy as np
 import math
+
+import numpy as np
+import talib
+
 
 # prints formatted price
 def formatPrice(n):
-	return ("-$" if n < 0 else "$") + "{0:.2f}".format(abs(n))
+    return ("-$" if n < 0 else "$") + "{0:.2f}".format(abs(n))
+
 
 # returns the vector containing stock data from a fixed file
 def getStockDataVec(key):
-	vec = []
-	lines = open("data/" + key + ".csv", "r").read().splitlines()
+    vec = []
+    lines = open("data/" + key + ".csv", "r").read().splitlines()
 
-	for line in lines[1:]:
-		vec.append(float(line.split(",")[4]))
+    for line in lines[1:]:
+        vec.append(float(line.split(",")[4]))
 
-	return vec
+    return vec
+
 
 # returns the sigmoid
 def sigmoid(x):
-	return 1 / (1 + math.exp(-x))
+    return 1 / (1 + math.exp(-x))
+
 
 # returns an an n-day state representation ending at time t
 def getState(data, t, n):
-	d = t - n + 1
-	block = data[d:t + 1] if d >= 0 else -d * [data[0]] + data[0:t + 1] # pad with t0
-	res = []
-	for i in xrange(n - 1):
-		res.append(sigmoid(block[i + 1] - block[i]))
+    d = t - n + 1
+    block = data[d:t + 1] if d >= 0 else -d * [data[0]] + data[0:t + 1]  # pad with t0
+    res = []
+    for i in xrange(n - 1):
+        res.append(sigmoid(block[i + 1] - block[i]))
 
-	return np.array([res])
+    return np.array([res])
+
+
+def get_state_rsi(data, t, n):
+    """
+
+    :param data:
+    :param t:
+    :param n:
+    :return:
+    """
+    item_data = np.array(data[t: t + n])
+    v1 = talib.RSI(item_data, timeperiod=int(n / 3))[-1]
+    v2 = talib.RSI(item_data, timeperiod=int(n / 3 * 2))[-1]
+    v3 = talib.RSI(item_data, timeperiod=n - 1)[-1]
+    # print int(n / 3), int(n / 3 * 2), n - 1
+    return np.array([[v1, v2, v3]]) / 100
+
+
+if __name__ == '__main__':
+    data = getStockDataVec('^GSPC')
+    print get_state_rsi(data, 0, 31)
+    print getState(data, 0, 10)
+
--- train.py
+++ train.py
@@ -9,7 +9,7 @@ if len(sys.argv) != 4:
 
 stock_name, window_size, episode_count = sys.argv[1], int(sys.argv[2]), int(sys.argv[3])
 
-agent = Agent(window_size, stock_name)
+agent = Agent(3, stock_name)
 data = getStockDataVec(stock_name)
 episode_count -= agent.episode
 l = len(data) - 1
@@ -17,16 +17,16 @@ batch_size = 32
 
 for e in xrange(episode_count + 1):
     print "Episode " + str(e) + "/" + str(episode_count)
-    state = getState(data, 0, window_size + 1)
+    state = get_state_rsi(data, 0, window_size + 1)
 
     total_profit = 0
     agent.inventory = []
 
-    for t in xrange(l):
+    for t in xrange(window_size, l):
         action = agent.act(state)
 
         # sit
-        next_state = getState(data, t + 1, window_size + 1)
+        next_state = get_state_rsi(data, t + 1, window_size + 1)
         reward = 0
 
         if action == 1:  # buy
@@ -44,12 +44,17 @@ for e in xrange(episode_count + 1):
         state = next_state
 
         if done:
+            for bought_price in agent.inventory:
+                total_profit += data[t] - bought_price
+            agent.inventory = []
             print "--------------------------------"
             print "Total Profit: " + formatPrice(total_profit)
             print "--------------------------------"
+            with open('Profit.txt', 'a') as f:
+                f.write("{0}\n".format(total_profit))
 
         if len(agent.memory) > batch_size:
             agent.expReplay(batch_size)
 
-    if e % 10 == 0:
+    if e % 1 == 0:
         agent.model.save("models/{0}_model_ep{1}".format(stock_name, e + agent.episode))
